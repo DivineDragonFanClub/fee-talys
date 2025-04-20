deba2d71b023f71757877f9990c50e746ab5b92f
Raytwo <raytwost@gmail.com>
2025-04-20 20:10:52 +0200

    Push all the things

---
+++ CustomMap/Assets/Code/MapSoundLabel.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenuAttribute]
+public class MapSoundLabel : ScriptableObject
+{
+	public string 起動音; // 0x18
+	public string 停止音; // 0x20
+	public string 破壊音; // 0x28
+}
